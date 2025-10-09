a130f96810fddbb3bced9fad6e5876acecb47e15
lengocvinh <lengocvinh271299@gmail.com>
2025-10-09 15:39:14 +0700

    change code cut pager

--- escposprinter/src/main/java/com/dantsu/escposprinter/EscPosPrinterCommands.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/EscPosPrinterCommands.java
@@ -732,7 +732,7 @@ public class EscPosPrinterCommands {
             return this;
         }
 
-        this.printerConnection.write(new byte[]{0x1D, 0x56, 0x01});
+        this.printerConnection.write(new byte[]{0x1D, 0x56, 66, 0x00});
         this.printerConnection.send(100);
         return this;
     }
