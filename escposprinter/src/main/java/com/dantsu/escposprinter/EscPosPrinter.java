dbd74dcafea50d821e26493b4ff812d471794508
lengocvinh <lengocvinh271299@gmail.com>
2025-10-13 17:35:19 +0700

    improve print image

--- escposprinter/src/main/java/com/dantsu/escposprinter/EscPosPrinter.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/EscPosPrinter.java
@@ -287,6 +287,7 @@ public class EscPosPrinter extends EscPosPrinterSize {
 
         // print image
         this.printer.printImage(cmd.toByteArray());
+        this.printer.feedPaper(100);
         this.printer.cutPaper();
 
         return this;
